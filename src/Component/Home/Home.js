8b43c64d9d8aea9767bbb60f6aa27a27607c4e35
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-10-12 12:06:55 +0600

    mount

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -9,7 +9,12 @@ const Home = () => {
 
     return (
         <div className='home-container'>
-            <h2>this is home{courses.length}</h2>
+            <div>
+                <h2>Notes!</h2>
+                <h2>If you take the quiz about your skills you know the status of your study.So You should take quiz regularly after the module finish</h2>
+                <p>Quizes are taken or analysis about your study not to harrase you</p>
+                <img src="./images/quisz.jpg" alt="" />
+            </div>
             <div className="course-container">
             {
                 courses.map(course=><Course 
--- src/Component/Option/Option.js
+++ src/Component/Option/Option.js
@@ -1,22 +1,23 @@
 import React from 'react';
+
 import './Option.css'
 
 const Option = (props) => {
     const {option,correctAnswer}=props;
     console.log(correctAnswer)
-    let ans;
+    
     const notify=(ans)=>alert(`${ans}`);
 
     const handleChecker=(selectedAnswer)=>{
         console.log(selectedAnswer)
         console.log(correctAnswer)
         if(selectedAnswer === correctAnswer){
-             ans = 'correct answer'
+            let ans = 'correct answer'
             notify(ans)
             
         }
         else{
-            ans = 'Wrong answer'
+           let ans = 'Wrong answer'
            notify(ans)
           
        }
@@ -24,7 +25,11 @@ const Option = (props) => {
     
     return (
         <div>
-            <input type="radio" name='option' onClick={()=>handleChecker(option)} /> <label htmlFor="">{option}</label>
+
+           <div>
+           <input type="radio" name='option' onClick={()=>handleChecker(option)} /> <label htmlFor="">{option}</label>
+           </div>
+           
             
             
         </div>
